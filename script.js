c2a52b00d934fede85cc4428cf098b67fe789a75
marinarose92 <maffia.marina@gmail.com>
2019-08-13 10:16:14 -0400

    created add, subtract, multiply and divide func

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (x, y) {
+    let sum = x + y;
+    return sum
+}
+
+function subtract (x, y) {
+    let difference = x - y;
+    return difference;
+}
+
+function multiply (x, y) {
+    let product = x * y;
+    return product;
+}
+
+function divide (x, y) {
+    let quotient = x / y;
+    return quotient;
+}
