0dacb7222e59dc345a6abd2764a8dc35ea6a45c6
marinarose92 <maffia.marina@gmail.com>
2019-08-13 15:38:59 -0400

    added calculatorLogic function

--- script.js
+++ script.js
@@ -1,55 +1,103 @@
-function add (num1, num2) {
-    let sum = num1 + num2;
-    console.log(sum)
+let myArr = [];
+
+function add(num1, num2) {
+  let sum = num1 + num2;
+  console.log(sum);
 }
 
-function subtract (num1, num2) {
-    let difference = num1 - num2;
-    console.log(difference)
+function subtract(num1, num2) {
+  let difference = num1 - num2;
+  console.log(difference);
 }
 
-function multiply (num1, num2) {
-    let product = num1 * num2;
-    console.log(product)
+function multiply(num1, num2) {
+  let product = num1 * num2;
+  console.log(product);
 }
 
-function divide (num1, num2) {
-    let quotient = num1 / num2;
-    console.log(quotient)
+function divide(num1, num2) {
+  let quotient = num1 / num2;
+  console.log(quotient);
 }
 
 function operate(operator, num1, num2) {
-    switch(operator) {
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case 'x':
-            multiply(num1, num2);
-            break;
-        case '%':
-            divide(num1, num2)
-            break;
-    }
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "x":
+      multiply(num1, num2);
+      break;
+    case "%":
+      divide(num1, num2);
+      break;
+  }
 }
 
-function renderButtons() {
-    let buttonsDiv = document.getElementById('buttonsDiv');
-    let calculatorButtonNames = [1, 2, 3, '%', 4, 5, 6, '×', 7, 8, 9, '-', '.', 0, '=', '+', 'Backspace', 'AC']
-    // x = 0;
-    // while (x <= 9) {
-        calculatorButtonNames.forEach(function(button) {
-            let calculatorButton = document.createElement('button');
-            buttonsDiv.appendChild(calculatorButton);
-            calculatorButton.classList.add('btn');
-            calculatorButton.innerText = button;
-            calculatorButton.id = calculatorButton.innerText;
-        })
-    }
-// }
+function renderDisplay() {
+  let screenSelector = document.getElementById("screen");
+  let buttonsDiv = document.getElementById("buttonsDiv");
+  let calculatorButtonNames = [
+    1,
+    2,
+    3,
+    "%",
+    4,
+    5,
+    6,
+    "×",
+    7,
+    8,
+    9,
+    "-",
+    ".",
+    0,
+    "=",
+    "+",
+    "Backspace",
+    "AC"
+  ];
+  calculatorButtonNames.forEach(function(button) {
+    let calculatorButton = document.createElement("button");
+    buttonsDiv.appendChild(calculatorButton);
+    calculatorButton.classList.add("btn");
+    calculatorButton.innerText = button;
+    calculatorButton.id = calculatorButton.innerText;
+  });
+  let calculatorButtonSelector = document.querySelectorAll(".btn");
+  populateDisplay(calculatorButtonSelector, screenSelector);
+}
 
-renderButtons();
+function populateDisplay(calculatorButtonSelector, screenSelector) {
+  console.log(calculatorButtonSelector);
+  calculatorButtonSelector.forEach(button => {
+    button.addEventListener("click", event => {
+      myArr.push(event.target.id);
+      screenSelector.innerText += event.target.id;
+      calculatorLogic();
+    });
+  });
+}
 
-console.log(operate('%', 5, 2));
+function calculatorLogic() {
+    equalButtonSelector = document.getElementById("=");
+    let num1, num2 = undefined;
+    console.log(myArr);
+    myArr.forEach(function(item) {
+        if (item === "+" || item === "-" || item === "×" || item === "÷") {
+            const operatorIndex = myArr.indexOf(item);
+            num1 = myArr.slice(0, operatorIndex).join('');
+            num2 = myArr.slice(operatorIndex + 1, -1).join('');
+        }
+    });
+
+    equalButtonSelector.addEventListener('click', event => {
+        console.log("I'm equal!");
+        operate('+', num1, num2);
+    })
+};
+
+renderDisplay();
